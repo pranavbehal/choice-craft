428bfc3b6591a1ae13bfdb46066d64b435453b51
pranavbehal <pranavbehal09@gmail.com>
2024-12-09 21:51:57 -0500

    Optimizing Script Loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +21,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="dark">
       {/* UserWay Accessibility Widget Implementaion for All Pages*/}
       <head>
-        <script
+        <Script
           src="https://cdn.userway.org/widget.js"
           data-account="FSY3fD98w9"
-        ></script>
+        ></Script>
       </head>
       <body className={inter.className} suppressHydrationWarning>
         <Providers>{children}</Providers>
